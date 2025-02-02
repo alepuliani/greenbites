808f642fedd89e1d3189006eb78c24fc3cf17091
alepuliani <151534738+alepuliani@users.noreply.github.com>
2025-02-02 15:09:28 +0100

    correzioni grid

--- src/pages/FavoriteRecipes.jsx
+++ src/pages/FavoriteRecipes.jsx
@@ -17,26 +17,26 @@ const FavoriteRecipes = () => {
 
   return (
     <div className="lg:px-36">
-      <h1 className="font-bold text-3xl px-7 capitalize my-7">
+      <h1 className="font-bold text-3xl px-7 capitalize my-7 text-center">
         favorite recipes{" "}
       </h1>
-      <div
-        className="grid grid-cols-2 
+      {favorites.length > 0 ? (
+        <div
+          className="grid grid-cols-2 
        md:grid-cols-3 lg:grid-cols-4 gap-3"
-      >
-        {favorites.length > 0 ? (
-          favorites.map((recipe) => {
+        >
+          {favorites.map((recipe) => {
             return (
               <div key={recipe.id} className="flex items-center justify-center">
                 {" "}
                 <RecipeCard recipe={recipe} />
               </div>
             )
-          })
-        ) : (
-          <p className="text-center">No favorite recipes yet</p>
-        )}
-      </div>
+          })}{" "}
+        </div>
+      ) : (
+        <p className="text-center">No favorite recipes yet</p>
+      )}
     </div>
   )
 }
